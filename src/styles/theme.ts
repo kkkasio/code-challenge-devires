1eb27d5485143e0d47a92524a4794dbaa4d8d32c
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-10-30 16:56:15 -0300

    feat: global styles and theme code style

---
+++ src/styles/theme.ts
@@ -0,0 +1,56 @@
+export default {
+  sizes: {
+    xsmall: '1.2rem',
+    small: '1.4rem',
+    medium: '1.6rem',
+    large: '1.8rem',
+    xlarge: '2rem',
+    xxlarge: '2.8rem',
+  },
+
+  colors: {
+    primary: '#F231A5',
+    secondary: '#3CD3C1',
+    mainBG: '#06092b',
+    white: '#FAFAFA',
+    black: '#030517',
+    lightGray: '#EAEAEA',
+    gray: '#8F8F8F',
+    darkGray: '#2E2F42',
+  },
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    ligth: 300,
+    normal: 400,
+    bold: 600,
+
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2rem',
+      xxlarge: '2.8rem',
+    },
+  },
+
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+  },
+
+  border: {
+    radius: '0.4rem',
+  },
+
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem',
+  },
+};
