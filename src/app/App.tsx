aa1e13099827b4530219e004f7869ef0e5ea2032
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-10-30 23:08:35 -0300

    fix: global styles on root app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,17 @@
 import Todo from 'pages/Todo';
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from 'styles/theme'
+import GlobalStyle from 'styles/global'
 
 function App() {
   return (
-    <div className="App">
-      <Todo />
-    </div>
+    <>
+     <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Todo />
+      </ThemeProvider>
+    </>
   );
 }
 
