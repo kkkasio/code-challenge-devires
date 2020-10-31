a03fc2caaf4feab9d9488fa7dcacddeaaef94d47
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-10-31 04:54:25 -0300

    refactor: button type (submit)

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,11 +6,12 @@ export type ButtonProps = {
   children?: React.ReactNode;
   size?: 'small' | 'medium' | 'large';
   icon?: React.ReactNode;
+
   onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 const Button = ({ children, icon, size = 'medium', ...rest }: ButtonProps) => (
-  <S.Wraper size={size} icon={!!icon} {...rest}>
+  <S.Wraper type="submit" size={size} icon={!!icon} {...rest}>
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </S.Wraper>
