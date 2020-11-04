6314b10a886f827c9ce95bfa464ff0c286c8eec1
Kásio Eduardo <kasioeduardo13@gmail.com>
2020-11-04 17:24:49 -0300

    fix: Stortbook Todo Page stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux';
+
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import store from 'store'
 
 export const decorators = [
   (Story) => (
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
     </ThemeProvider>
+    </Provider>
   )
 ]
